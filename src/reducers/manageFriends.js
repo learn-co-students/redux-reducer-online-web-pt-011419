576298f370f9ee00f45302a243c2cc9311fa5953
dclements9 <dclements@protonmail.com>
2019-10-21 11:30:23 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(previousState = {friends: []}, action){
+    switch (action.type){
+        case 'ADD_FRIEND':
+                console.log(previousState)
+            return ('add')
+        case 'REMOVE_FRIEND':
+                console.log(friend)
+            return ('remove')
+        default:
+            return previousState
+    }
 }
